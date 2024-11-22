63ce9f90c8b3705907669dd0529ac0059256c490
NicolaAdrah <niccolo1996.na@gmail.com>
2024-11-22 16:34:02 +0100

    Moving files && Adding utilities.c

---
+++ utilities.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <ljmd.h>
+
+/* helper function: get current time in seconds since epoch */
+
+static double wallclock()
+{
+        struct timeval t;
+        gettimeofday(&t,0);
+        return ((double) t.tv_sec) + 1.0e-6*((double) t.tv_usec);
+}
+
+/* helper function: zero out an array */
+static void azzero(double *d, const int n)
+{
+    int i;
+    for (i=0; i<n; ++i) {
+        d[i]=0.0;
+    }
+}
+
+/* helper function: apply minimum image convention */
+static double pbc(double x, const double boxby2)
+{
+    while (x >  boxby2) x -= 2.0*boxby2;
+    while (x < -boxby2) x += 2.0*boxby2;
+    return x;
+}
+
+/* compute kinetic energy */
+static void ekin(mdsys_t *sys)
+{
+    int i;
+
+    sys->ekin=0.0;
+    for (i=0; i<sys->natoms; ++i) {
+        sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
+    }
+    sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
+}
