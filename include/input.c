8e3fbe39bcea2c85f4a345fcc66f96f6b8db2377
NicolaAdrah <niccolo1996.na@gmail.com>
2024-11-22 16:42:33 +0100

    Adding the files as in include folder to include branch

---
+++ include/input.c
@@ -0,0 +1,31 @@
+// input.c
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "ljmd.h"
+
+static int get_a_line(FILE *fp, char *buf) {
+    char tmp[BLEN], *ptr;
+
+    if (fgets(tmp, BLEN, fp)) {
+        int i;
+        ptr = strchr(tmp, '#');
+        if (ptr) *ptr = '\0';
+        i = strlen(tmp); --i;
+        while (isspace(tmp[i])) {
+            tmp[i] = '\0';
+            --i;
+        }
+        ptr = tmp;
+        while (isspace(*ptr)) { ++ptr; }
+        i = strlen(ptr);
+        strcpy(buf, tmp);
+        return 0;
+    } else {
+        perror("problem reading input");
+        return -1;
+    }
+}
+
+
