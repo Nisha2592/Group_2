0d711c1a71c4b08c9ad5c60eac39345dbcce3041
christian_hpc <ticachristian20@gmail.com>
2024-11-23 00:00:21 +0800

    edited main.c

--- main.c
+++ main.c
@@ -12,6 +12,16 @@
 #include <math.h>
 #include <sys/time.h>
 
+
+/*generic file- or pathname buffer length*/
+#define BLEN 200
+
+
+/*a few physical constants*/
+const double kboltz=0.0019872067;  /* boltzman constant in kcal/mol/K */
+const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
+
+
 /* main */
 int main(int argc, char **argv)
 {
