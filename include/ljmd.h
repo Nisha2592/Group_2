3d7827fa26f6eb6a81dfef99e927f57448357e60
christian_hpc <ticachristian20@gmail.com>
2024-11-24 01:32:16 +0800

    updated the force.c and removed verlet.c

---
+++ include/ljmd.h
@@ -0,0 +1,51 @@
+#ifndef C_HEADERS
+
+#define C_HEADERS 
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+
+
+/* generic file- or pathname buffer length */
+#define BLEN 200
+
+extern const double kboltz;
+extern const double mvsq2e;
+
+/* structure to hold the complete information
+ * about the MD system */
+struct _mdsys{ 
+    int natoms,nfi,nsteps;
+    double dt, mass, epsilon, sigma, box, rcut;
+    double ekin, epot, temp;
+    double *rx, *ry, *rz;
+    double *vx, *vy, *vz;
+    double *fx, *fy, *fz;
+};
+typedef struct _mdsys mdsys_t;
+
+extern double wallclock();
+
+extern void azzero(double *d, const int n);
+
+extern double pbc(double x, const double boxby2);
+
+extern void ekin(mdsys_t *sys);
+
+extern void force(mdsys_t *sys);
+
+extern void velverlet_step1(mdsys_t *sys);
+
+extern void velverlet_step2(mdsys_t *sys);
+
+extern void output(mdsys_t *sys, FILE *erg, FILE *traj);
+
+extern int get_a_line(FILE *fp, char *buf);
+
+extern void cleanup(mdsys_t *sys);
+
+#endif
---
+++ include/utilities.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include "ljmd.h"
+
+
+//const double kboltz=0.0019872067;
+//const double mvsq2e=2390.05736153349;
+extern const double kboltz;
+extern const double mvsq2e;
+/* helper function: get current time in seconds since epoch */
+
+
+double wallclock()
+{
+        struct timeval t;
+        gettimeofday(&t,0);
+        return ((double) t.tv_sec) + 1.0e-6*((double) t.tv_usec);
+}
+
+/* helper function: zero out an array */
+void azzero(double *d, const int n)
+{
+    int i;
+    for (i=0; i<n; ++i) {
+        d[i]=0.0;
+    }
+}
+
+/* helper function: apply minimum image convention */
+double pbc(double x, const double boxby2)
+{
+    while (x >  boxby2) x -= 2.0*boxby2;
+    while (x < -boxby2) x += 2.0*boxby2;
+    return x;
+}
+
+/* compute kinetic energy */
+void ekin(mdsys_t *sys)
+{
+    int i;
+
+    sys->ekin=0.0;
+    for (i=0; i<sys->natoms; ++i) {
+        sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
+    }
+    sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
+}
