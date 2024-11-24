3f56fcd6ef2c92ffdb4d86cd028abc48d97bedff
NicolaAdrah <niccolo1996.na@gmail.com>
2024-11-24 17:27:56 +0100

    Applying 1st Optimization, Newton 3rd law. Changing force.c file in include branch

--- include/force.c
+++ include/force.c
@@ -36,62 +36,7 @@ void velverlet_step2(mdsys_t *sys) {
 
 
 /* compute forces */
-// void force(mdsys_t *sys)
-// {
-//     double r,ffac, c12, c6;
-//     double rx,ry,rz, rcsq, rsq;
-//     int i,j;
-
-//     /* zero energy and forces */
-//     sys->epot=0.0;
-//     azzero(sys->fx,sys->natoms);
-//     azzero(sys->fy,sys->natoms);
-//     azzero(sys->fz,sys->natoms);
-
-//     // --------------- OPTIMIZATION ATTEMPT II ---------------
-//     // Removing expensive math using
-//     c12 = 4.0 * sys->epsilon * pow(sys->sigma,12.0);
-//     c6 = 4.0 * sys->epsilon * pow(sys->sigma, 6.0);
-//     rcsq = sys->rcut * sys->rcut;
-
-//     // --------------- OPTIMIZATION ATTEMPT I -----------------
-//     // Newton 3rd law
-//     for(i=0; i < (sys->natoms) - 1; ++i) {
-//         for(j= i + 1; j < (sys->natoms); ++j) {
-
-//             /* particles have no interactions with themselves */
-//             // if (i==j) continue;
-
-//             /* get distance between particle i and j */
-//             rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
-//             ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
-//             rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-//             rsq = rx*rx + ry*ry + rz*rz;
-
-
-//             /* compute force and energy if within cutoff */
-//             if (rsq < rcsq) {
-//                 double r6, rinv;
-//                 rinv = 1.0/rsq;
-//                 r6 = rinv * rinv * rinv;
-//                 ffac = (12.0 * c12 * r6 - 6.0 * c6) * r6 * rinv;
-//                 sys->epot += r6 * (c12 * r6 - c6);
-
-//                 // Consider action in i direction,
-//                 sys->fx[i] += rx*ffac;
-//                 sys->fy[i] += ry*ffac;
-//                 sys->fz[i] += rz*ffac;
-//                 // then the reaction in the j direction.
-//                 sys->fx[i] -= rx*ffac;
-//                 sys->fy[i] -= ry*ffac;
-//                 sys->fz[i] -= rz*ffac;
-//             }
-//         }
-//     }
-// }
-
-
-void force(mdsys_t *sys)
+void force(mdsys_t *sys) 
 {
     double r,ffac;
     double rx,ry,rz;
@@ -103,29 +48,29 @@ void force(mdsys_t *sys)
     azzero(sys->fy,sys->natoms);
     azzero(sys->fz,sys->natoms);
 
-    for(i=0; i < (sys->natoms); ++i) {
-        for(j=0; j < (sys->natoms); ++j) {
-
-            /* particles have no interactions with themselves */
-            if (i==j) continue;
+    for(i=0; i < (sys->natoms)-1; ++i) {
+        for(j=i+1; j < (sys->natoms); ++j) {
 
             /* get distance between particle i and j */
             rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
             ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
             rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
             r = sqrt(rx*rx + ry*ry + rz*rz);
-
+      
             /* compute force and energy if within cutoff */
             if (r < sys->rcut) {
                 ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
                                          +6*pow(sys->sigma/r,6.0)/r);
-
-                sys->epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
+                
+                sys->epot += 4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
                                                -pow(sys->sigma/r,6.0));
 
                 sys->fx[i] += rx/r*ffac;
                 sys->fy[i] += ry/r*ffac;
                 sys->fz[i] += rz/r*ffac;
+                sys->fx[j] -= rx/r*ffac;
+                sys->fy[j] -= ry/r*ffac;
+                sys->fz[j] -= rz/r*ffac;
             }
         }
     }
