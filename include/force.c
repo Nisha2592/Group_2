dbe712c50e0c7c13376f1b7758249a48f06b0498
NicolaAdrah <niccolo1996.na@gmail.com>
2024-11-24 18:13:11 +0100

    Applying 2nd Optimization, avoiding expensive calculations. Changing force.c file in include branch

--- include/force.c
+++ include/force.c
@@ -38,39 +38,53 @@ void velverlet_step2(mdsys_t *sys) {
 /* compute forces */
 void force(mdsys_t *sys) 
 {
-    double r,ffac;
-    double rx,ry,rz;
-    int i,j;
+    double r, ffac;
+    double rx, ry, rz, rcsq, rsq;
+    double c12, c6;
+    int i, j;
 
     /* zero energy and forces */
-    sys->epot=0.0;
-    azzero(sys->fx,sys->natoms);
-    azzero(sys->fy,sys->natoms);
-    azzero(sys->fz,sys->natoms);
+    sys->epot = 0.0;
+    azzero(sys->fx, sys->natoms);
+    azzero(sys->fy, sys->natoms);
+    azzero(sys->fz, sys->natoms);
 
-    for(i=0; i < (sys->natoms)-1; ++i) {
-        for(j=i+1; j < (sys->natoms); ++j) {
+    /* precompute some constants */
+    c12 = 4.0 * sys->epsilon * pow(sys->sigma, 12.0);
+    c6 = 4.0 * sys->epsilon * pow(sys->sigma, 6.0);
+    rcsq = sys->rcut * sys->rcut;
+
+    for (i = 0; i < (sys->natoms) - 1; ++i) {
+        for (j = i + 1; j < (sys->natoms); ++j) {
 
             /* get distance between particle i and j */
-            rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
-            ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
-            rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-            r = sqrt(rx*rx + ry*ry + rz*rz);
-      
-            /* compute force and energy if within cutoff */
-            if (r < sys->rcut) {
-                ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
-                                         +6*pow(sys->sigma/r,6.0)/r);
-                
-                sys->epot += 4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
-                                               -pow(sys->sigma/r,6.0));
+            rx = pbc(sys->rx[i] - sys->rx[j], 0.5 * sys->box);
+            ry = pbc(sys->ry[i] - sys->ry[j], 0.5 * sys->box);
+            rz = pbc(sys->rz[i] - sys->rz[j], 0.5 * sys->box);
+            rsq = rx * rx + ry * ry + rz * rz;
 
-                sys->fx[i] += rx/r*ffac;
-                sys->fy[i] += ry/r*ffac;
-                sys->fz[i] += rz/r*ffac;
-                sys->fx[j] -= rx/r*ffac;
-                sys->fy[j] -= ry/r*ffac;
-                sys->fz[j] -= rz/r*ffac;
+            /* compute force and energy if within cutoff */
+            if (rsq < rcsq) {
+                double r2inv, r6inv, r12inv;
+                
+                // Precompute reciprocal of distance squared
+                r2inv = 1.0 / rsq;
+                r6inv = r2inv * r2inv * r2inv; // r^(-6)
+                r12inv = r6inv * r6inv;        // r^(-12)
+                    
+                // Force factor
+                ffac = (12.0 * c12 * r12inv - 6.0 * c6 * r6inv) * r2inv;
+
+                // Accumulate potential energy
+                sys->epot += c12 * r12inv - c6 * r6inv;
+
+                // Update forces
+                sys->fx[i] += rx * ffac;
+                sys->fy[i] += ry * ffac;
+                sys->fz[i] += rz * ffac;
+                sys->fx[j] -= rx * ffac;
+                sys->fy[j] -= ry * ffac;
+                sys->fz[j] -= rz * ffac;
             }
         }
     }
