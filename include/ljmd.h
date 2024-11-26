08adbdf4a61ee9415c933e171c2cf47bb93eedcd
NicolaAdrah <niccolo1996.na@gmail.com>
2024-11-27 00:07:54 +0100

    Defining the function that have been moved from utilities.c with inline attribute

--- include/ljmd.h
+++ include/ljmd.h
@@ -30,9 +30,24 @@ typedef struct _mdsys mdsys_t;
 
 extern double wallclock();
 
-extern void azzero(double *d, const int n);
+/* helper function: zero out an array */
+static __attribute__((always_inline))
+void azzero(double *d, const int n)
+{
+    int i;
+    for (i=0; i<n; ++i) {
+        d[i]=0.0;
+    }
+}
 
-extern double pbc(double x, const double boxby2, const double Box);
+/* helper function: apply minimum image convention */
+static __attribute__((always_inline,pure))
+double pbc(double x, const double boxby2, const double Box)
+{
+    while (x >  boxby2) x -= Box;
+    while (x < -boxby2) x += Box;
+    return x;
+}
 
 extern void ekin(mdsys_t *sys);
 
