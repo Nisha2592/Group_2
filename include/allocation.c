0c114f6dd4dde33766e262123ae551de501bb138
NicolaAdrah <niccolo1996.na@gmail.com>
2024-11-27 12:27:07 +0100

    Adding the allocation.c source file. Change development branch

---
+++ include/allocation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "ljmd.h"
+
+void allocation(mdsys_t *sys)
+{
+    sys->rx=(double *)malloc(sys->natoms*sizeof(double));
+    sys->ry=(double *)malloc(sys->natoms*sizeof(double));
+    sys->rz=(double *)malloc(sys->natoms*sizeof(double));
+    sys->vx=(double *)malloc(sys->natoms*sizeof(double));
+    sys->vy=(double *)malloc(sys->natoms*sizeof(double));
+    sys->vz=(double *)malloc(sys->natoms*sizeof(double));
+    sys->fx=(double *)malloc(sys->natoms*sizeof(double));
+    sys->fy=(double *)malloc(sys->natoms*sizeof(double));
+    sys->fz=(double *)malloc(sys->natoms*sizeof(double));
+}
