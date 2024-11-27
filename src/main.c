411f313fe719f387c2fe17c975c4563a4a6ec1f4
NicolaAdrah <niccolo1996.na@gmail.com>
2024-11-27 12:08:30 +0100

    Adding reading function that include the input function. Change main.c in development branch

--- src/main.c
+++ src/main.c
@@ -29,27 +29,7 @@ int main(int argc, char **argv)
     t_start = wallclock();
 
     /* read input file */
-    if(get_a_line(stdin,line)) return 1;
-    sys.natoms=atoi(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.mass=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.epsilon=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.sigma=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.rcut=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.box=atof(line);
-    if(get_a_line(stdin,restfile)) return 1;
-    if(get_a_line(stdin,trajfile)) return 1;
-    if(get_a_line(stdin,ergfile)) return 1;
-    if(get_a_line(stdin,line)) return 1;
-    sys.nsteps=atoi(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.dt=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    nprint=atoi(line);
+    reading(line, &sys, restfile, trajfile, ergfile, &nprint);
 
     /* allocate memory */
     sys.rx=(double *)malloc(sys.natoms*sizeof(double));
